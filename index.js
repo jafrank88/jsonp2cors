8d1dc646d912de027b75aa273ac58735ce937212
Jonathan Franklin <jafrank@uw.edu>
2022-03-08 16:01:41 -0800

    trying to fix callback

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ app.get('/api/CL/', (req, res) => {
     const cloutput3 = cloutput2.replaceAll('/opinion/', 'https://www.courtlistener.com/opinion/')
     const output = JSON.parse(cloutput3)
     console.log(output)
-    res.set('content-type', 'application/json');
+    res.set('content-type', 'application/json',
+    'jsonp callback name', cluserrequest
+    );
     res.jsonp(output);
   });
 });
