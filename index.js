3d9ad604962b7704edbdfc3e4d243cb4b61ab3ea
Jonathan Franklin <jafrank@uw.edu>
2022-03-15 12:18:14 -0700

    trying red.writeheader

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/api/CL/', (req, res, next) => {
     const cloutput2 = cloutput.replace('"next"', '"perpage":5, "next"')
     const cloutput3 = cloutput2.replaceAll('/opinion/', 'https://www.courtlistener.com/opinion/')
     const output = JSON.parse(cloutput3)
-    res.type('content-type', 'application/json;')
+    res.writeHeader(200, { 'Content-Type': 'application/json' })
     console.log(output)
     res.json(output);
   });
