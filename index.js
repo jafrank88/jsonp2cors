8f6934aea874771d1e6a3bf58157c3c2f5e65065
Jonathan Franklin <jafrank@uw.edu>
2022-03-16 21:50:09 -0700

    Switched to writeheader

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/api/CL/', (req, res, next) => {
     //console.log ('postResType :'+ res.get('Content-Type'))
     const output = JSON.parse(cloutput3)
     //console.log ('postParse :'+ res.get('Content-Type'))
-    res.set('Content-Type', 'application/json');
+    res.writeHeader('Content-Type', 'application/json')
     console.log ('Final Content-Type :'+ res.get('Content-Type'))
     //console.log(output.headers)
     const str =  clcallback + '( \' ' + JSON.stringify(output) + ' \' ) ;';//jsonp
