c9810fea49c1e4157ca9842c503663e69b279b98
Jonathan Franklin <jafrank@uw.edu>
2022-03-30 10:41:47 -0700

    trying stringify again

--- index.js
+++ index.js
@@ -85,8 +85,7 @@ app.get('/api/CL/', (req, res, next) => {
          //   console.log(jfresp)
             res
              .writeHead(200,{'Content-Type': 'application/json'})
-             .end(clcallback + JSON.parse(response.data))
-         
+             .end(clcallback + JSON.stringify(response.data))
       })
     });
 
