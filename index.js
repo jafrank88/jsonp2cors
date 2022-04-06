046536b0ebdbd99205c6a77459e3db2c797b0ea8
Jonathan Franklin <jafrank@uw.edu>
2022-04-06 12:36:53 -0700

    fixing CAP to get title to appear

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/api/CAP/', (req, res) => {
       const capresp2 = capresp1.replace('"next"', '"perpage":5, "next"')
       const capresp3 = capresp2.replaceAll('"url"', '"uurl"')
       const capresp4 = capresp3.replaceAll('"frontend_url"', '"url"')
-      const capout = capresp4.replaceAll('"case"','"title"')
+      const capout = capresp4.replaceAll('"name_abbreviation"','"title"')
       console.log(capout)
        res
        .type('application/javascript')
