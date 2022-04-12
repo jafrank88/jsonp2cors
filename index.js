ad88051e008749b8b2f82aca5d1c47e921374977
Jonathan Franklin <jafrank@uw.edu>
2022-04-12 14:57:00 -0700

    tweaked URL for GOOG

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/api/GOOG/', (req, res) => {
   let wacCallback = req.query.callback
   console.log('GOOG UserRequest ' + wacUserRequest)
   console.log('GOOG Callback : ' + wacCallback)
-  const resp = axios.get('https://www.googleapis.com/customsearch/v1?q=' + wacUserRequest + '&alt=json&cx=e59140f1ca4f44214&key=AIzaSyAan8PHJ6Ji5S2r7S7iQiFWIwcn6K3ijL4')
+  const resp = axios.get('https://www.googleapis.com/customsearch/v1/?alt=json&cx=e59140f1ca4f44214&key=AIzaSyAan8PHJ6Ji5S2r7S7iQiFWIwcn6K3ijL4&q=' + wacUserRequest )
   .then(function (response) {
     const wacresp = JSON.stringify(response.data)
     const wacresp1 = wacresp.replaceAll('"totalResults"', '"total_results"')
