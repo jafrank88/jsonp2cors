8609294ea33b6c5bffebf5f4862cc8bdb2742c5d
Jonathan Franklin <jafrank@uw.edu>
2022-05-16 12:58:32 -0700

    passing auth as axios header

--- index.js
+++ index.js
@@ -110,8 +110,10 @@ app.get('/api/CAP/', (req, res) => {
       res.end();
     } else {
     let DCcallback = req.query.callback
-    request.setHeader('authorization', 'tIom76bl0l0FGyokkyAhN7GnlgjqmVBxPjF/CoMUAMY=')
-    axios.get('https://content-out.bepress.com/v2/digitalcommons.law.uw.edu/query?limit=5&q=' + DCuserrequest)
+    axios.get('https://content-out.bepress.com/v2/digitalcommons.law.uw.edu/query?limit=5&q=' + DCuserrequest,{
+      headers: {
+        "authorization":"tIom76bl0l0FGyokkyAhN7GnlgjqmVBxPjF/CoMUAMY="
+      }})
       .then(function (response) {
         const capresp = JSON.stringify(response.data)
         const capresp1 = capresp.replace('"total_hits"', '"total_results"')
