b058d9e6f75b9521f487addc240f4248c70fb5cb
emiliearenkid <43757349+emiliearenkid@users.noreply.github.com>
2023-03-27 16:53:29 -0700

    Update index.js
    Seeing whether not replacing total_hits and changing the value has any effect.

--- index.js
+++ index.js
@@ -125,7 +125,8 @@ app.get('/api/CAP/', (req, res) => {
 // University of Washington Law Digital Commons
 // For some reason, won't always display the right results. If it does display, then they are correct, but
 // it is either all 5 or none, even if there are results to view.
-// It adds a "total_results":5 to the end of the json rather than editing the total_results at the beginning
+// It adds a "total_results":5 or "total_hits":5 to the end of the json rather than 
+// editing the total_results at the beginning, like how the replace does.
 app.get('/api/UWDC/', (req, res) => {
   let DCuserrequest = req.query.q;
   if (DCuserrequest == "") {
@@ -146,8 +147,8 @@ app.get('/api/UWDC/', (req, res) => {
       }*/
       response.data.total_hits = 5;
       //response.data.total_results = 5;
-      let capresp = JSON.stringify(response.data);
-      let capresp1 = capresp.replace('"total_hits"', '"total_results"');
+      let capresp1 = JSON.stringify(response.data);
+      //let capresp1 = capresp.replace('"total_hits"', '"total_results"');
       let capresp2 = capresp1.replace('"limit"', '"perpage"');
       //let capresp3 = capresp2.replace('"query_meta":{', '');
       //let capresp4 = capresp2.replace(/"download_format"[\s]*\][\s]*\}/m, '"download_format"]}');
