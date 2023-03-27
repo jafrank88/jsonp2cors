65f6b4a45f6a8d2b803335afc30fd1086239c100
emiliearenkid <43757349+emiliearenkid@users.noreply.github.com>
2023-03-27 16:24:48 -0700

    Test whether UWDC capresp1 works

--- index.js
+++ index.js
@@ -71,14 +71,6 @@ app.get('/api/CL/', (req, res) => {
           }
           response.data.results = results;
         }
-        /*if (response.data.total_results > 5) {
-          response.data.total_results = 5;
-          results = [{}, {}, {}, {}, {}];
-          for (let i = 0; i < 5; i++) {
-            results[i] = response.data.results[i];
-          }
-          response.data.results = results;
-        }*/
         let clresp = JSON.stringify(response.data);
         let cloutput = clresp.replace('"count"', '"total_results"');
         let cloutput2 = cloutput.replace('"next"', '"perpage":5,"next"');
@@ -131,6 +123,9 @@ app.get('/api/CAP/', (req, res) => {
 });
 
 // University of Washington Law Digital Commons
+// For some reason, won't always display the right results. If it does display, then they are correct, but
+// it is either all 5 or none, even if there are results to view.
+// It adds a "total_results:5" to the end of the json rather than editing the total_results at the beginning
 app.get('/api/UWDC/', (req, res) => {
   let DCuserrequest = req.query.q;
   if (DCuserrequest == "") {
@@ -150,9 +145,9 @@ app.get('/api/UWDC/', (req, res) => {
         response.data.total_results = 5;
       }*/
       //response.data.total_hits = 5;
-      response.data.total_results = 5;
-      let capresp = JSON.stringify(response.data);
-      let capresp1 = capresp.replace('"total_hits"', '"total_results"');
+      //response.data.total_results = 5;
+      let capresp1 = JSON.stringify(response.data);
+      //let capresp1 = capresp.replace('"total_hits"', '"total_results"');
       let capresp2 = capresp1.replace('"limit"', '"perpage"');
       //let capresp3 = capresp2.replace('"query_meta":{', '');
       //let capresp4 = capresp2.replace(/"download_format"[\s]*\][\s]*\}/m, '"download_format"]}');
